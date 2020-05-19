64f5232419d1bae8f00c6a8979e9287a78b23190
Yenny Marcela Montoya Gomez <yenny.montoya@globant.com>
2020-05-19 18:22:21 -0500

    Changing the message displayed

--- src/test/java/PageObject/Guru99HomePage.java
+++ src/test/java/PageObject/Guru99HomePage.java
@@ -4,6 +4,6 @@ package PageObject;
 public class Guru99HomePage {
 
     public static void main (String[] args) {
-        System.out.println("hola GitHub");
+        System.out.println("Hey Git!");
     }
 }
