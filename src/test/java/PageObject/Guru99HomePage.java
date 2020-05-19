3af4569c15283ea58ba813686083123849714fe1
camilo.avila@believeapps.co <camilo.avila@believeapps.co>
2020-05-19 18:07:26 -0500

    Modificar el texto de hola mundo a Hola github

--- src/test/java/PageObject/Guru99HomePage.java
+++ src/test/java/PageObject/Guru99HomePage.java
@@ -4,6 +4,6 @@ package PageObject;
 public class Guru99HomePage {
 
     public static void main (String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola GitHub");
     }
 }
