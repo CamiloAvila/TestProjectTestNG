afd4700391ba450c095f767d67cd21c479d65744
camilo.avila@believeapps.co <camilo.avila@believeapps.co>
2020-05-26 17:57:14 -0500

    add log message

--- src/test/java/PageObject/Guru99Login.java
+++ src/test/java/PageObject/Guru99Login.java
@@ -6,6 +6,7 @@ public class Guru99Login {
 
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"HOLIIIIIII MUNDO");
+        System.err.println("Hola Error");
     }
 
 }
