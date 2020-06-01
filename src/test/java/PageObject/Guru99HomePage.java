ad85202a0dbe1dd8e1706ffd26cf322f880fd835
camilo.avila@believeapps.co <camilo.avila@believeapps.co>
2020-06-01 16:29:41 -0500

    Branch testing

--- src/test/java/PageObject/Guru99HomePage.java
+++ src/test/java/PageObject/Guru99HomePage.java
@@ -5,5 +5,6 @@ public class Guru99HomePage {
 
     public static void main (String[] args) {
         System.out.println("hola GitHub test");
+        System.err.println("This is a error message");
     }
 }
